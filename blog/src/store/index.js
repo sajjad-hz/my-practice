71c4c912c53875e0dafa1887116faa18298f9bd1
sah <sajjad.heidarzadeh@adorsys.com>
2023-10-19 22:18:24 +0200

    modify sequelize

--- blog/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2'
-
-const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USER,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT
-})
