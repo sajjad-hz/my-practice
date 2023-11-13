80743d2d42ee852a3fd45aa71771b479583fe0c8
sah <sajjad.heidarzadeh@adorsys.com>
2023-11-13 15:09:13 +0100

    upadate err-handler for api

--- blog/src/app.js
+++ blog/src/app.js
@@ -8,7 +8,7 @@ import auth from "./middlewares/auth";
 import session from "express-session";
 import RedisStore from "connect-redis";
 import Redis from "ioredis";
-import {createClient} from "redis";
+import { createClient } from "redis";
 
 export async function bootstrap() {
   const app = express();
@@ -27,12 +27,11 @@ export async function bootstrap() {
 
   // const redisClient = new Redis(6383);
   // const store = new RedisStore({ client: redisClient});
-  let redisClient = createClient()
-  redisClient.connect().catch(console.error)
+  let redisClient = createClient();
+  redisClient.connect().catch(console.error);
   let store = new RedisStore({
     client: redisClient,
-    
-  })
+  });
 
   app.use(
     session({
--- blog/src/middlewares/error-handler.js
+++ blog/src/middlewares/error-handler.js
@@ -1,16 +1,23 @@
 export default (err, req, res, next) => {
-  console.log(err.message)
+  console.log(err.message);
   // console.log(err.status)
 
-  const status = err.status ?? 500
+  const status = err.status ?? 500;
 
   const message =
-    err.status < 500 || process.env.NODE_ENV === 'development'
+    err.status < 500 || process.env.NODE_ENV === "development"
       ? err.message
-      : `Server Error: Please call to admin.`
+      : `Server Error: Please call to admin.`;
 
-  res.status(status).render('error', {
-    title: `Error ${status}`,
-    content: message
-  })
-}
+  if (req.url.startsWith("/api")) {
+    res.status(status).json({
+      code: status,
+      message,
+    });
+  } else {
+    res.status(status).render("error", {
+      title: `Error ${status}`,
+      content: message,
+    });
+  }
+};
